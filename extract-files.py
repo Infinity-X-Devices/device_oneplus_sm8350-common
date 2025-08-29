2d2df7f3cfc3475468235691d0b9dc94ee61b83c
Debarpan102 <debarpan102github@gmail.com>
2025-08-29 16:09:14 +0530

    sm8350-common: Update alarm component to LA.QSSI.15.0.r1-16100-qssi.0
    Signed-off-by: Debarpan102 <debarpan102github@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -51,8 +51,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('user system', 'user root'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
-    'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
-        .apktool_patch('blob-patches/PowerOffAlarm.patch'),
     'system_ext/bin/wfdservice': blob_fixup()
         .add_needed('libwfdservice_shim.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
