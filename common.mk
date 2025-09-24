4bfe08ab46383bcedc076ea77f570f8622e088c7
Debarpan102 <debarpan102github@gmail.com>
2025-09-25 02:25:21 +0530

    Revert "sm8350-common: Avoid building oplus doze"
    This reverts commit 937052918d852226c6b20ec0c28146acf2668ab0.

--- common.mk
+++ common.mk
@@ -130,6 +130,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
     dalvik.vm.image-dex2oat-threads=8
 
+# Doze
+PRODUCT_PACKAGES += \
+    OplusDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
